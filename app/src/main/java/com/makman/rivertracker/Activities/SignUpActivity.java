af665423d66515fe423a9eb56a196b45e6eb8765
Sam <sammakman@gmail.com>
2016-03-30 22:22:56 -0700

    Added Login Activity

---
+++ app/src/main/java/com/makman/rivertracker/Activities/SignUpActivity.java
@@ -0,0 +1,56 @@
+package com.makman.rivertracker.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.makman.rivertracker.FavoritesActivity;
+import com.makman.rivertracker.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Bind(R.id.signup_edit_text_email)
+    EditText mEmail;
+
+    @Bind(R.id.signup_edit_text_password)
+    EditText mPassword;
+
+    @Bind(R.id.signup_edit_text_password_confirm)
+    EditText mConfirm;
+
+    @Bind(R.id.signup_button_signup)
+    Button mSignup;
+
+    @Bind(R.id.signup_button_have_account)
+    Button mLogin;
+
+    @Bind(R.id.signup_button_no_account)
+    Button mContinue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.signup_button_signup) void signup(){
+        
+    }
+
+    @OnClick(R.id.signup_button_have_account) void login(){
+
+    }
+
+    @OnClick(R.id.signup_button_no_account) void tryItOut(){
+        Intent intent = new Intent(SignUpActivity.this, FavoritesActivity.class);
+        startActivity(intent);
+    }
+
+}

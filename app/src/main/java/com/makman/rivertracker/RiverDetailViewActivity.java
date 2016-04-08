1fd2d8928d30c4ccb5d38bf009a545c52385ff0d
Tyler Moody <tmoody@lclark.edu>
2016-04-07 18:04:47 -0700

    fixed crash on opening riverdetailview

--- app/src/main/java/com/makman/rivertracker/RiverDetailViewActivity.java
+++ app/src/main/java/com/makman/rivertracker/RiverDetailViewActivity.java
@@ -13,24 +13,25 @@ import com.makman.rivertracker.Fragments.RiversFragment;
 import com.makman.rivertracker.NetworkTasks.RiverDetailNetworkTask;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class RiverDetailViewActivity extends AppCompatActivity implements RiverDetailNetworkTask.RiverDetailNetworkTaskListener {
     private static final String TAG = RiverDetailViewActivity.class.getSimpleName();
     @Bind(R.id.river_details_alerts_textview)
-    private TextView mAlertsTextview;
+    TextView mAlertsTextview;
     @Bind(R.id.river_details_image_view)
-    private ImageView mRiverImage;
+    ImageView mRiverImage;
     @Bind(R.id.river_details_section_textview)
-    private TextView mRiverSection;
+    TextView mRiverSection;
     @Bind(R.id.river_details_difficulty_textview)
-    private TextView mDifficulty;
+    TextView mDifficulty;
     @Bind(R.id.river_details_state_textview)
-    private TextView mState;
+    TextView mState;
     @Bind(R.id.river_details_flowrate_textview)
-    private TextView mFlowrate;
+    TextView mFlowrate;
     @Bind(R.id.river_details_description_textview)
-    private TextView mRiverDescription;
+    TextView mRiverDescription;
 
 
     @OnClick(R.id.river_details_favorite_button) void OnClick(){
@@ -47,7 +48,7 @@ public class RiverDetailViewActivity extends AppCompatActivity implements RiverD
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_river_detail_view);
-
+        ButterKnife.bind(this);
         river = getIntent().getExtras().getParcelable(RiversFragment.ARG_RIVER);
         if(river!=null){
             ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
@@ -67,6 +68,9 @@ public class RiverDetailViewActivity extends AppCompatActivity implements RiverD
     @Override
     public void PostExecute(River river) {
         Log.d(TAG, "post execute");
+        if(river==null){
+            return;
+        }
         if(river.getSection()!=null) {
             mRiverSection.setText(river.getSection());
         }

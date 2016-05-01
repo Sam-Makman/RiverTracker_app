1c32741bc330acca31d78e126eafbee947583c2c
Sam <sammakman@gmail.com>
2016-04-30 21:28:41 -0700

    Added okhttp

--- app/src/main/java/com/makman/rivertracker/NetworkTasks/VolleyNetworkTask.java
+++ app/src/main/java/com/makman/rivertracker/NetworkTasks/VolleyNetworkTask.java
@@ -1,14 +1,27 @@
 package com.makman.rivertracker.NetworkTasks;
 
 import android.app.Application;
+import android.text.format.Time;
+import android.util.Log;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.squareup.okhttp.Cache;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.picasso.OkHttpDownloader;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 
 /**
- * Created by prog on 4/13/16.
+ * Created by Sam.
  */
 public class VolleyNetworkTask extends Application {
+    public static final int HTTP_RESPONSE_DISK_CACHE_MAX_SIZE = 200 * 1024 * 1024;
+    private static final String TAG = VolleyNetworkTask.class.getSimpleName();
     private static VolleyNetworkTask sInstance;
     private RequestQueue mRequestQueue;
 
@@ -17,6 +30,11 @@ public class VolleyNetworkTask extends Application {
         super.onCreate();
         mRequestQueue = Volley.newRequestQueue(this);
         sInstance = this;
+        Picasso.Builder builder = new Picasso.Builder(this);
+        builder.downloader(new OkHttpDownloader(this, Integer.MAX_VALUE));
+        final Picasso built = builder.build();
+//        built.setIndicatorsEnabled(true);
+        Picasso.setSingletonInstance(built);
     }
 
     public synchronized static VolleyNetworkTask getInstance() {
